b59d00d182d682113b56e38538f0aec1b17f80a0
Jan Gerber <j@xiph.org>
2005-08-06 16:47:58 +0000

    - rework autotools system to work with latest version of ffmpeg-cvs   [now uses pkg-config files provided by ffmpeg to determine dependencies]

--- get_ffmpeg_cvs.sh
+++ get_ffmpeg_cvs.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
 cvs -z3 -d:pserver:anonymous@cvs.mplayerhq.hu:/cvsroot/ffmpeg/ co ffmpeg
+cd ffmpeg
+./configure --enable-libogg --enable-theora --disable-mp3lame --enable-a52 --enable-pthreads --enable-vhook --enable-dv1394 --enable-gpl
+make 
