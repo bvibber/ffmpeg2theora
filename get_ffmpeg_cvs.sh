71750e96d3e13b4124959de47d3f96fa86bc8ddc
Ralph Giles <giles@xiph.org>
2006-01-08 05:58:30 +0000

    Update the ffmpeg configure options for current cvs. Still doesn't build though.

--- get_ffmpeg_cvs.sh
+++ get_ffmpeg_cvs.sh
@@ -4,5 +4,5 @@
 #extra="--enable-faad --enable-dts --enable-libgsm --enable-amr_nb --enable-amr_nb-fixed --enable-amr_wb --enable-amr_if2"
 
 cvs -z3 -d:pserver:anonymous@cvs.mplayerhq.hu:/cvsroot/ffmpeg co ffmpeg
-cd ffmpeg && ./configure --enable-libogg --enable-theora --enable-a52 --enable-pthreads --enable-gpl --disable-encoders  --disable-muxes $extra && make
+cd ffmpeg && ./configure --enable-libogg --enable-theora --enable-a52 --enable-pthreads --enable-gpl --disable-encoders  --disable-muxers $extra && make
 make 
