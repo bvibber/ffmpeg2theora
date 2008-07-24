cc5f9bc112d2be39ce1eb80a1de549842bc9ec79
Jan Gerber <j@xiph.org>
2008-07-24 14:01:54 +0000

    fix help, its --intpufps

--- src/ffmpeg2theora.c
+++ src/ffmpeg2theora.c
@@ -1215,7 +1215,7 @@ void print_usage (){
         "    ffmpeg2theora frame%%06d.png -o output.ogv\n"
         "\n"
         "  Live streaming from V4L Device:\n"
-        "    ffmpeg2theora /dev/video0 -f video4linux -fps 15 -x 160 -y 128 -o - \\\n"
+        "    ffmpeg2theora /dev/video0 -f video4linux --inputfps 15 -x 160 -y 128 -o - \\\n"
         "     | oggfwd iccast2server 8000 password /theora.ogv\n"
         "\n"
         "  Live encoding from a DV camcorder (needs a fast machine):\n"
